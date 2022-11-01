3604ad01792e57a944e4a8a8a70a2120c90cd9a4
Alex Kozhenov <kozhenovalex@gmail.com>
2022-11-01 04:43:29 +0300

    add create elements with coordinates

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -31,27 +31,50 @@ container.appendChild(buttons);
 // Create field
 
 let sizeField = 4;
+let arrayElements = [];
+let emptyElement = {};
+
 const mainField = document.createElement('div');
 mainField.classList.add('main-field');
 
 function createField(sizeField) {
   mainField.innerHTML = "";
 
-  for(let i = 1; i <= sizeField * sizeField; i++) {
-    const item = document.createElement('div');
-    item.classList.add('item');
-    if (document.body.clientWidth >= 720) {
-      item.style.width = (500 - 15*sizeField - 1)/sizeField - 2 + "px";
-      item.style.height = (500 - 15*sizeField - 1)/sizeField - 2 + "px";
-    } else {
-      item.style.width = (300 - 5*sizeField - 1)/sizeField - 2 + "px";
-      item.style.height = (300 - 5*sizeField - 1)/sizeField - 2 + "px";
-    }
+  createArrayElements(sizeField);
+
+  for (let y = 0; y < sizeField; y++) {
     
-    item.style.margin = "5px";
-    item.innerHTML = i;
-    mainField.appendChild(item);
+    for (let x = 0; x < sizeField; x++) {
+      const item = document.createElement('div');
+      item.classList.add('item');
+
+      let size;
+      if (document.body.clientWidth >= 720) {
+        size = 500/sizeField;
+      } else {
+        size = 300/sizeField;
+      }
+      item.style.width = size + "px";
+      item.style.height = size + "px";
+      item.style.left = x * size + "px";
+      item.style.top = y * size + "px";
+
+      item.innerHTML = arrayElements[y][x];
+      if (arrayElements[y][x] === sizeField*sizeField) {
+        emptyElement.x = x;
+        emptyElement.y = y;
+        item.style.visibility = "hidden";
+      }
+
+      item.addEventListener('click', () => {
+        console.log(x, y);
+      });
+
+      mainField.appendChild(item);
+    }
+
   }
+
 }
 
 createField(sizeField);
@@ -59,9 +82,13 @@ container.appendChild(mainField);
 
 // Change size
 
-const changeSize = `<div>Frame size: ${sizeField}x${sizeField}</div>`;
+const frameSize = document.createElement('div');
+frameSize.innerHTML = `Frame size: ${sizeField}x${sizeField}`;
+container.appendChild(frameSize);
 
-container.insertAdjacentHTML('beforeend', changeSize)
+function changeFrameSize (sizeField) {
+  frameSize.innerHTML = `Frame size: ${sizeField}x${sizeField}`;
+}
 
 const otherSizes = document.createElement('div');
 otherSizes.classList.add('other-sizes');
@@ -72,16 +99,39 @@ for (let i = 3; i < 9; i++) {
   let sizeEl = document.createElement('div');
   sizeEl.classList.add('other-item');
   sizeEl.innerHTML = sizeText;
-  sizeEl.href = "#";
+
   sizeEl.addEventListener('click', () => {
     sizeField = i;
     createField(sizeField);
+    changeFrameSize(sizeField);
   });
   otherSizes.append(sizeEl, " ");
 }
 
 container.appendChild(otherSizes);
 
-const width = window.width;
+// Shuffle
 
-console.log(document.body.clientWidth);
+function createArrayElements(sizeField) {
+  let arrayNumbers = [];
+  let countNumbers = sizeField*sizeField;
+  for (let i = 1; i <= countNumbers; i++) {
+    arrayNumbers.push(i);
+  }
+  arrayNumbers.sort((a, b) => Math.random() - 0.5);
+
+  arrayElements = [];
+
+  for (let y = 0; y < sizeField; y++) {
+    let arrayRow = [];
+    for (let x = 0; x < sizeField; x++) {
+      arrayRow.push(arrayNumbers[countNumbers - 1]);
+      countNumbers--;
+    }
+
+    arrayElements.push(arrayRow);
+  }
+  
+  console.log(arrayElements);
+}
+
